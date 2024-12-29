fad7f4930b9abf5d7ae36da924d701f23f16769a
JokerTheBond <simor98@gmail.com>
2024-12-29 19:22:33 +0100

    Create TokenizedUniswapV3Deployer

--- contracts/extensions/interfaces/ITokenizedUniswapV3Factory.sol
+++ contracts/extensions/interfaces/ITokenizedUniswapV3Factory.sol
@@ -5,6 +5,7 @@ interface ITokenizedUniswapV3Factory {
 	event NFTLPCreated(address indexed token0, address indexed token1, address NFTLP, uint);
 	
 	function uniswapV3Factory() external view returns (address);
+	function deployer() external view returns (address);
 	function getNFTLP(address tokenA, address tokenB) external view returns (address);
 	function allNFTLP(uint) external view returns (address);
 	function allNFTLPLength() external view returns (uint);
