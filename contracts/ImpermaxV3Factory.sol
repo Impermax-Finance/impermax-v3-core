74c85cf7f51a5397f84757b575e1449c23bddf68
JokerTheBond <simor98@gmail.com>
2025-01-28 13:52:33 +0100

    Remove redundant code

--- contracts/ImpermaxV3Factory.sol
+++ contracts/ImpermaxV3Factory.sol
@@ -51,7 +51,6 @@ contract ImpermaxV3Factory is IFactory {
 	}
 	
 	function createCollateral(address nftlp) external returns (address collateral) {
-		_getTokens(nftlp);
 		require(getLendingPool[nftlp].collateral == address(0), "Impermax: ALREADY_EXISTS");		
 		collateral = cDeployer.deployCollateral(nftlp);
 		ICollateral(collateral)._setFactory();
@@ -60,7 +59,6 @@ contract ImpermaxV3Factory is IFactory {
 	}
 	
 	function createBorrowable0(address nftlp) external returns (address borrowable0) {
-		_getTokens(nftlp);
 		require(getLendingPool[nftlp].borrowable0 == address(0), "Impermax: ALREADY_EXISTS");		
 		borrowable0 = bDeployer.deployBorrowable(nftlp, 0);
 		IBorrowable(borrowable0)._setFactory();
@@ -69,7 +67,6 @@ contract ImpermaxV3Factory is IFactory {
 	}
 	
 	function createBorrowable1(address nftlp) external returns (address borrowable1) {
-		_getTokens(nftlp);
 		require(getLendingPool[nftlp].borrowable1 == address(0), "Impermax: ALREADY_EXISTS");		
 		borrowable1 = bDeployer.deployBorrowable(nftlp, 1);
 		IBorrowable(borrowable1)._setFactory();
