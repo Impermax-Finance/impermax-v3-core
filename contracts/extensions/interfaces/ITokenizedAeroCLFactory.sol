903f6b8ba9506bae76a4cff8e3b70d6c5151e6db
JokerTheBond <simor98@gmail.com>
2025-07-21 00:28:27 +0200

    Aerodrome draft

---
+++ contracts/extensions/interfaces/ITokenizedAeroCLFactory.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.0;
+pragma experimental ABIEncoderV2;
+
+interface ITokenizedAeroCLFactory {
+	event NFTLPCreated(address indexed token0, address indexed token1, address NFTLP, uint);
+	event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin);
+	event NewAdmin(address oldAdmin, address newAdmin);
+	
+	function admin() external view returns (address);
+	function pendingAdmin() external view returns (address);
+	
+	function clFactory() external view returns (address);
+	function nfpManager() external view returns (address);
+	function deployer() external view returns (address);
+	function oracle() external view returns (address);
+	function rewardToken() external view returns (address);
+	
+	function getNFTLP(address tokenA, address tokenB) external view returns (address);
+	function allNFTLP(uint) external view returns (address);
+	function allNFTLPLength() external view returns (uint);
+	
+	function createNFTLP(address tokenA, address tokenB) external returns (address NFTLP);
+}
---
+++ contracts/extensions/interfaces/ITokenizedAeroCLPosition.sol
@@ -0,0 +1,67 @@
+pragma solidity >=0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "../../interfaces/INFTLP.sol";
+
+interface ITokenizedAeroCLPosition {
+	
+	// ERC-721
+	
+	function name() external view returns (string memory);
+	function symbol() external view returns (string memory);
+	function balanceOf(address owner) external view returns (uint256 balance);
+	function ownerOf(uint256 tokenId) external view returns (address owner);
+	function getApproved(uint256 tokenId) external view returns (address operator);
+	function isApprovedForAll(address owner, address operator) external view returns (bool);
+	
+	function DOMAIN_SEPARATOR() external view returns (bytes32);
+	function nonces(uint256 tokenId) external view returns (uint256);
+	
+	function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+	function safeTransferFrom(address from, address to, uint256 tokenId) external;
+	function transferFrom(address from, address to, uint256 tokenId) external;
+	function approve(address to, uint256 tokenId) external;
+	function setApprovalForAll(address operator, bool approved) external;
+	function permit(address spender, uint tokenId, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+	
+	// INFTLP
+	
+	function token0() external view returns (address);
+	function token1() external view returns (address);
+	function getPositionData(uint256 _tokenId, uint256 _safetyMarginSqrt) external returns (
+		uint256 priceSqrtX96,
+		INFTLP.RealXYs memory realXYs
+	);
+	
+	function split(uint256 tokenId, uint256 percentage) external returns (uint256 newTokenId);
+	
+	// ITokenizedAeroCLPosition
+	
+	function factory() external view returns (address);
+	function clFactory() external view returns (address);
+	function nfpManager() external view returns (address);
+	function oracle() external view returns (address);
+	function rewardToken() external view returns (address);
+	
+	function getPool(int24 tickSpacing) external view returns (address pool);
+	function getGauge(int24 tickSpacing) external view returns (address gauge);
+	
+	function oraclePriceSqrtX96() external returns (uint256);
+	
+	event SyncReward(uint256 totalRewardBalance);
+	event UpdatePositionReward(uint256 indexed tokenId, uint256 rewardOwed, uint256 claimAmount);
+	event GaugeAdded(int24 tickSpacing, address gauge);
+
+	function _initialize (
+		address _clFactory, 
+		address _nfpManager, 
+		address _oracle, 
+		address _token0, 
+		address _token1,
+		address _rewardToken
+	) external;
+	
+	function mint(address to, uint256 tokenId, bytes calldata data) external;
+	function redeem(address to, uint256 tokenId) external;	
+	function claim(uint256 tokenId, address to) external returns (uint256 claimAmount);
+}
